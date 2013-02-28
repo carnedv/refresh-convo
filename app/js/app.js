d2bd7952951f2036dbf2a07d01c0fe3b4b2202b2
dvc <davec@ashesandater.com>
2013-02-28 08:43:57 -0600

    Started creating convo app.

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,9 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+angular.module('convoTrack', ['convoTrack.filters', 'convoTrack.services', 'convoTrack.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.when('/', {templateUrl: 'partials/index.html', controller: IndexController});
+    $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: AboutController});
+    $routeProvider.otherwise({redirectTo: '/'});
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,10 +3,17 @@
 /* Controllers */
 
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
-
-
-function MyCtrl2() {
+function IndexController($scope, $route) {
+	$scope.navSelected = 'home';
+	$scope.conversationMessages = [
+		{ id: 1, username: 'Foo', name: 'Angular.js', url: 'http://angular.js/', description: 'A client-side, MVC Javascript framework.', createdOn: '2/28/2012 18:40'},
+		{ id: 2, username: 'Bar', name: 'Socket.io', url: 'http://socket.io/', description: 'Real-time http transport mechanism written in Javascript.', createdOn: '2/28/2012 18:40'}
+	];
 }
-MyCtrl2.$inject = [];
+//MyCtrl1.$inject = [];
+
+
+function AboutController($scope, $route) {
+	$scope.navSelected = 'about';
+}
+//MyCtrl2.$inject = [];
