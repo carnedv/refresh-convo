0f99de436f4d5ab6df4ce0249bcc65cd96db886a
Dave Carner <dave@velocity2.com>
2013-02-28 10:25:37 -0600

    Worked on Add New Topic submission process. Allowed for one-off usernames (need to validate).

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,18 +2,53 @@
 
 /* Controllers */
 
-
 function IndexController($scope, $route) {
 	$scope.navSelected = 'home';
-	$scope.conversationMessages = [
-		{ id: 1, username: 'Foo', name: 'Angular.js', url: 'http://angular.js/', description: 'A client-side, MVC Javascript framework.', createdOn: '2/28/2012 18:40'},
-		{ id: 2, username: 'Bar', name: 'Socket.io', url: 'http://socket.io/', description: 'Real-time http transport mechanism written in Javascript.', createdOn: '2/28/2012 18:40'}
+
+	$scope.messages = [
+		{ id: 1, username: 'Foo', name: 'Angular.js', url: 'http://angularjs.org/', description: 'A client-side, MVC Javascript framework.', createdOn: new Date() },
+		{ id: 2, username: 'Bar', name: 'Socket.io', url: 'http://socket.io/', description: 'Real-time http transport mechanism written in Javascript.', createdOn: new Date() }
 	];
+
+	$scope.users = [];
+
+	$scope.messages.forEach(function (msg)
+	{
+		if ($scope.users.indexOf(msg.username))
+		{
+			$scope.users.push(msg.username);
+		}
+	});
+
+	var newMessage = {
+		username: null,
+		name: null,
+		url: null,
+		description: null,
+		createdOn: null,
+		newUsername: null
+	};
+
+	$scope.newMessage = angular.copy(newMessage);
+
+	$scope.saveNewMessage = function()
+	{
+		$scope.newMessage.createdOn = new Date();
+
+		if (!$scope.newMessage.username && $scope.newMessage.newUsername)
+		{
+			$scope.newMessage.username = $scope.newMessage.newUsername;
+			$scope.users.push($scope.newMessage.username);
+		}
+
+		delete $scope.newMessage.newUsername;
+
+		$scope.messages.unshift($scope.newMessage);
+		$scope.newMessage = angular.copy(newMessage);
+	};
 }
-//MyCtrl1.$inject = [];
 
 
 function AboutController($scope, $route) {
 	$scope.navSelected = 'about';
 }
-//MyCtrl2.$inject = [];
